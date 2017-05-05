8fd2e833d51df68feed56ca8952197083abfeb23
Dario Trbovic <timmy.dario@gmail.com>
2017-05-05 23:43:45 +0200

    composer updated

--- resources/views/ajax-test.blade.php
+++ resources/views/ajax-test.blade.php
@@ -1,5 +1,8 @@
-<form name="ajaxform" id="ajaxform" action="http://blitzer.loc/api/msg/send/2016-01-03_13:25:00" method="POST">
+<form name="ajaxform" id="ajaxform" action="http://blitzer.loc/api/msg/send/2016-01-03_13:25:00?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjQsImlzcyI6Imh0dHA6XC9cL2JsaXR6ZXIubG9jXC9hcGlcL2F1dGhcL2xvZ2luIiwiaWF0IjoxNDkzODQyNDgzLCJleHAiOjIxNDc0ODM2NDcsIm5iZiI6MTQ5Mzg0MjQ4MywianRpIjoiUU9DN245UHVmN0E5RDIwZCJ9.NWyubrQjdKPM7BGfdXnUQ_-lpC2NRWZ8qVn6z3ggyJo" method="POST">
     <input type="submit" id="submit">
+    <input type="hidden" name="user_id" value="5">
+    <input type="hidden" name="reply" value="test">
+    <input type="hidden" name="pin_id" value="24">
 </form>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -20,10 +23,11 @@ $(function () {
                       },
                     success:function(data, textStatus, jqXHR)
                     {
-                        console.log(jqXHR);
+                        console.log(data);
                     },
                     error: function(jqXHR, textStatus, errorThrown)
                     {
+                        console.log(errorThrown);
                         //if fails
                     }
                 });
