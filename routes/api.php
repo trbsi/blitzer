f8c04265d0800b2f574794fb7349e689078b100c
Dario Trbovic <timmy.dario@gmail.com>
2017-06-07 12:18:24 +0200

    use URLs with GET params

--- routes/api.php
+++ routes/api.php
@@ -22,14 +22,14 @@ $api->version('v1', function (Router $api) {
             $api->post('save-push-token', ['uses' => 'App\Api\V1\Controllers\BootstrapController@updateNotificationToken']);
 
             $api->group(['prefix' => 'map'], function (Router $api) {
-                $api->get('pins/{lat}/{lng}/{current_time}', 'App\Api\V1\Controllers\Map\MapController@pins');
+                $api->get('pins', 'App\Api\V1\Controllers\Map\MapController@pins');
                 $api->get('tags', 'App\Api\V1\Controllers\Map\MapController@tags');
-                $api->post('pins/{lat}/{lng}/{current_time}', 'App\Api\V1\Controllers\Map\MapController@pinPublish');
+                $api->post('pins', 'App\Api\V1\Controllers\Map\MapController@pinPublish');
             });
 
             $api->group(['prefix' => 'msg'], function (Router $api) {
-                $api->get('view/{pin_id}/{user_id}', 'App\Api\V1\Controllers\Messages\MessageController@view');
-                $api->post('send/{current_time}', 'App\Api\V1\Controllers\Messages\MessageController@send');
+                $api->get('view', 'App\Api\V1\Controllers\Messages\MessageController@view');
+                $api->post('send', 'App\Api\V1\Controllers\Messages\MessageController@send');
             });
         });        
     });
