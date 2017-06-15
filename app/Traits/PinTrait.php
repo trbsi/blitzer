8e430a68a51cfdfeab51b0aef6e45d5ae05f19e2
Dario Trbovic <timmy.dario@gmail.com>
2017-06-15 14:15:57 +0200

    improved fake pins

--- app/Traits/PinTrait.php
+++ app/Traits/PinTrait.php
@@ -39,6 +39,7 @@ trait PinTrait
         //decode html
 
         $i = 0;
+        $tags = [];
         foreach ($pin->relationPinTag as $pin2) {
             $tags[$i]["tag_id"]   = $pin2->tag_id;
             $tags[$i]["tag_name"] = $pin2->relationTag->tag;
@@ -93,7 +94,7 @@ trait PinTrait
         $pins = CacheHelper::getCache("fake_pins", ["location" => round($lat + $lng)]);
 
         if (!empty($pins)) {
-             return $pins;
+            // return $pins;
         }
 
         if (!empty($pin)) {
@@ -718,6 +719,16 @@ trait PinTrait
             }
         }
 
+        foreach ($data["tags"][$get][$i] as $key => $value) {
+            $value["tag_id"] = 0;
+            $tags[] = $value;
+        }
+
+        $comment = "";
+        if(env('APP_ENV') != 'live') {
+            $comment = rand(0,1)%2 ? "" : $this->randomizer('{Please|Just} make this {cool|awesome|random} test sentence {rotate {quickly|fast} and random|spin and be random}');
+        }
+
         return
             [
             "user" =>
@@ -731,14 +742,90 @@ trait PinTrait
             "pin"  =>
             [
                 "publish_time" => $date->format('Y-m-d H:i:s'),
-                "comment"      => "",
+                "comment"      => $comment,
                 "lat"          => $lat,
                 "lng"          => $lng,
                 "pin_id"       => $data["pin_id"],
-                "tags"         => $data["tags"][$get][$i],
+                "tags"         => $tags,
             ],
         ];
     }
+
+    private function randomizer($sentence){
+        $rand_sentence = '';
+     
+        // Split sentence into characters
+        $char_array = str_split($sentence);
+     
+        // Open braces counter
+        $br_count = 0;
+     
+        // Opening left brace offset
+        $br_start = 0;
+     
+        // Iterate over characters
+        foreach ($char_array as $key => $char) {
+            switch ($char){
+     
+                // Is it left brace?
+                case '{':
+                    // Is it opening brace?
+                    if($br_count == 0){
+                        // If it is note the offset
+                        $br_start = $key;
+                    }
+     
+                    // Increment open braces counter
+                    $br_count++;
+                    break;
+     
+                    // Is it right brace?
+                case '}':
+                    // Decrement open braces counter
+                    $br_count--;
+     
+                    // Is it closing brace?
+                    if($br_count == 0) {
+                        /* Call randomizer again to detect nested
+                         * braces in the current section. Randomizer
+                         * returns current section but without any
+                         * nested braces so we can proceed with
+                         * exploding it using | character */
+                        $rands = explode(
+                            '|',
+                            $this->randomizer(
+                                implode(
+                                    '',
+                                    array_slice(
+                                        $char_array,
+                                        $br_start + 1,
+                                        $key - $br_start - 1
+                                    )
+                                )
+                            )
+                        );
+     
+                        /* Store random element of current section's
+                         * | exploded array */
+                        $rand_sentence .= $rands[array_rand($rands)];
+                    }
+                    break;
+     
+                    // Is it any other character?
+                default:
+                    /* Since | is only inside {} delimited section,
+                     * if there aren't any open braces we can just store
+                     * other chracters. */
+     
+                    if($br_count == 0){
+                        $rand_sentence .= $char;
+                    }
+            }
+        }
+     
+        return $rand_sentence;
+    }
+
     //****************************FAKE PINS END********************************
 
 }
